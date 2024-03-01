e122123d0229408d04b90c072133f576afb43c5a
prueba2@gmail.com <prueba@gmail.com>
2024-02-29 19:49:09 -0500

    Creacion del Controlador REST con Spring

--- src/main/java/gm/inventarios/modelo/Producto.java
+++ src/main/java/gm/inventarios/modelo/Producto.java
@@ -5,10 +5,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
